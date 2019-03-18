576da71c2b058aeecf6713cbee5db59a4fd11f9d
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2019-03-18 10:26:51 -0300

    update - build 2

--- src/services/environments/environment-config.js
+++ src/services/environments/environment-config.js
@@ -2,7 +2,7 @@ export const environmentConfig = {
    invercolProd: {
       production: true,
       environment: 'PROD',
-      //apiUrl: 'http://invercolbackend.publicidadorigen.cl',
-      apiUrl: 'http://localhost:80',
+      apiUrl: 'http://invercolbackend.publicidadorigen.cl',
+      //apiUrl: 'http://localhost:80',
    },
 }
